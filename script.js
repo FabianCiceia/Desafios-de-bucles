fecc301fb4395e3a2d572ab759a7ae7bc6c37510
FabianCiceia <ciceiafabian@gmail.com>
2024-01-30 13:04:39 -0300

    Resolviendo los desafios

---
+++ script.js
@@ -0,0 +1,28 @@
+// Imprimir impares 1-20    
+for(var i = 1 ; i <20;i+=2){
+    console.log(i);
+}
+
+// Disminuir múltiplos de 3
+for(var i = 99; i > 0; i-=3){
+    console.log(i);
+}
+
+//Imprime la secuencia
+for(var i=4; i >= -3.5; i-=1.5){
+    console.log(i)
+}
+
+// Sigma 
+let sum = 0;
+for(let i = 0; i <= 100; i++){
+    sum+=i;
+}
+console.log(sum);
+
+// Factorial 
+let producty = 1;
+for(let i = 1; i<=12; i++){
+    producty*=i;
+}
+console.log(producty);
